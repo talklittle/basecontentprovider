aeee8618e503c8a614b23b37cb95c2ee0c7a2c75
Andrew Shu <talklittle@gmail.com>
2018-11-10 17:23:56 -0800

    helper: disable write-ahead logging API 28

--- library/src/main/java/com/talklittle/basecontentprovider/BaseDatabaseHelper.java
+++ library/src/main/java/com/talklittle/basecontentprovider/BaseDatabaseHelper.java
@@ -3,6 +3,7 @@ package com.talklittle.basecontentprovider;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 import android.util.Log;
 
 public abstract class BaseDatabaseHelper extends SQLiteOpenHelper {
@@ -11,6 +12,17 @@ public abstract class BaseDatabaseHelper extends SQLiteOpenHelper {
 
     public BaseDatabaseHelper(Context context, String databaseName, int databaseVersion) {
         super(context, databaseName, null, databaseVersion);
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // Android P (9.0) enables write-ahead logging by default, breaking the contract in the
+            // framework docs, so revert it to disabled
+            db.disableWriteAheadLogging();
+        }
     }
 
     @Override
